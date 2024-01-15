e4d211cdc1a5056113fa860fc8b03ca84d719606
HuanMeng-official <3099044113@qq.com>
2024-01-15 17:55:31 +0800

    upload files

---
+++ src/main/java/me/huanmeng/RespawnEye.java
@@ -0,0 +1,16 @@
+package me.huanmeng;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RespawnEye implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("respawneye");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
