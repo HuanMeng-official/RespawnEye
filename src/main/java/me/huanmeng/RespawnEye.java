7a4451992d2d3a7e30e7d330a1108280e954bb0e
HuanMeng-official <3099044113@qq.com>
2024-01-28 13:38:46 +0800

    简化注册，重写物品栏

---
+++ src/main/java/me/huanmeng/RespawnEye.java
@@ -0,0 +1,21 @@
+package me.huanmeng;
+
+import me.huanmeng.world.groups.ItemGroups;
+import me.huanmeng.world.items.ReItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RespawnEye implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("Respawneye");
+	public static final String MODID = "respawneye";
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+		ReItems.registryItems();
+		ItemGroups.registryGroup();
+	}
+}
