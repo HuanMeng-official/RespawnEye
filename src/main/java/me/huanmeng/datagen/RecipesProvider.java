7fff58981bda0e52140b49b69f9fba1584b39233
HuanMeng-official <3099044113@qq.com>
2024-05-04 21:37:33 +0800

    添加铀，钛的锭，粒，块配方

--- src/main/java/me/huanmeng/datagen/RecipesProvider.java
+++ src/main/java/me/huanmeng/datagen/RecipesProvider.java
@@ -29,6 +29,8 @@ public class RecipesProvider extends FabricRecipeProvider {
         offerBlasting(exporter, TIGER_EYE_SMELTABLES, RecipeCategory.MISC, ReItems.TIGERS_EYE_INGOT, 0.4f, 100, "tigers_eye");
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ReItems.RUBY, RecipeCategory.DECORATIONS, ReBlocks.RUBY_BLOCK);
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ReItems.TIGERS_EYE_INGOT, RecipeCategory.DECORATIONS, ReBlocks.TIGERS_EYE_BLOCK);
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ReItems.TITANIUM_INGOT, RecipeCategory.DECORATIONS, ReBlocks.TITANIUM_BLOCK);
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ReItems.URANIUM_INGOT, RecipeCategory.DECORATIONS, ReBlocks.URANIUM_BLOCK);
 
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReTools.RUBY_AXE, 1).pattern("RR ").pattern("RI ").pattern(" I ").input('R', ReItems.RUBY).input('I', Items.STICK).criterion(hasItem(ReItems.RUBY), conditionsFromItem(ReItems.RUBY)).offerTo(exporter, new Identifier(getRecipeName(ReTools.RUBY_AXE)));
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReTools.RUBY_PICKAXE, 1).pattern("RRR").pattern(" I ").pattern(" I ").input('R', ReItems.RUBY).input('I', Items.STICK).criterion(hasItem(ReItems.RUBY), conditionsFromItem(ReItems.RUBY)).offerTo(exporter, new Identifier(getRecipeName(ReTools.RUBY_PICKAXE)));
@@ -53,8 +55,13 @@ public class RecipesProvider extends FabricRecipeProvider {
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReTools.ULTIMATE_HOE, 1).pattern("TT ").pattern(" U ").pattern(" I ").input('T', ReItems.TITANIUM_INGOT).input('U', ReItems.URANIUM_INGOT).input('I', Items.IRON_INGOT).criterion(hasItem(ReItems.TITANIUM_NUGGET), conditionsFromItem(ReItems.TITANIUM_INGOT)).offerTo(exporter, new Identifier(getRecipeName(ReTools.ULTIMATE_HOE)));
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReTools.ULTIMATE_SHOVEL, 1).pattern(" T ").pattern(" U ").pattern(" I ").input('T', ReItems.TITANIUM_INGOT).input('U', ReItems.URANIUM_INGOT).input('I', Items.IRON_INGOT).criterion(hasItem(ReItems.TITANIUM_NUGGET), conditionsFromItem(ReItems.TITANIUM_INGOT)).offerTo(exporter, new Identifier(getRecipeName(ReTools.ULTIMATE_SHOVEL)));
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReTools.ULTIMATE_SWORD, 1).pattern(" U ").pattern(" T ").pattern(" I ").input('T', ReItems.TITANIUM_INGOT).input('U', ReItems.URANIUM_INGOT).input('I', Items.IRON_INGOT).criterion(hasItem(ReItems.TITANIUM_NUGGET), conditionsFromItem(ReItems.TITANIUM_INGOT)).offerTo(exporter, new Identifier(getRecipeName(ReTools.ULTIMATE_SWORD)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReItems.TITANIUM_INGOT, 1).pattern("###").pattern("###").pattern("###").input('#', ReItems.TITANIUM_NUGGET).criterion(hasItem(ReItems.TITANIUM_NUGGET), conditionsFromItem(ReItems.TITANIUM_INGOT)).offerTo(exporter, new Identifier(MODID + "titanium_ingot_form_titanium_nugget"));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReItems.TITANIUM_NUGGET, 9).pattern("#").input('#', ReItems.TITANIUM_INGOT).criterion(hasItem(ReItems.TITANIUM_NUGGET), conditionsFromItem(ReItems.TITANIUM_INGOT)).offerTo(exporter, new Identifier(MODID + "titanium_nugget_form_titanium_ingot"));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReItems.URANIUM_INGOT, 1).pattern("###").pattern("###").pattern("###").input('#', ReItems.URANIUM_NUGGET).criterion(hasItem(ReItems.URANIUM_INGOT), conditionsFromItem(ReItems.URANIUM_NUGGET)).offerTo(exporter, new Identifier(MODID + "uranium_ingot_form_uranium_nugget"));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ReItems.URANIUM_NUGGET, 9).pattern("#").input('#', ReItems.URANIUM_INGOT).criterion(hasItem(ReItems.URANIUM_INGOT), conditionsFromItem(ReItems.URANIUM_NUGGET)).offerTo(exporter, new Identifier(MODID + "uranium_nugget_form_uranium_ingot"));
     }
 
+    private static final String MODID = "respawneye:";
     private static final List<ItemConvertible> RUBY_SMELTABLES = List.of(ReBlocks.RUBY_ORE, ReBlocks.DEEPSLATE_RUBY_ORE);
     private static final List<ItemConvertible> TIGER_EYE_SMELTABLES = List.of(ReBlocks.TIGERS_EYE_ORE, ReBlocks.DEEPSLATE_TIGERS_EYE_ORE, ReItems.RAW_TIGERS_EYE);
 }
