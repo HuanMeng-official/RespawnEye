7fa587d958a5edcd4ab57728559e5fa32d6af22e
HuanMeng-official <3099044113@qq.com>
2024-06-07 23:05:33 +0800

    Ultimate Bow起作用了（指拉弓动画）

--- src/main/java/me/huanmeng/util/ModelsPredicateProviderRegistry.java
+++ src/main/java/me/huanmeng/util/ModelsPredicateProviderRegistry.java
@@ -3,7 +3,6 @@ package me.huanmeng.util;
 import me.huanmeng.item.ReTools;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class ModelsPredicateProviderRegistry {
@@ -13,7 +12,7 @@ public class ModelsPredicateProviderRegistry {
     }
 
     public static void registerBow(Item bow) {
-        ModelPredicateProviderRegistry.register(Items.BOW, new Identifier("pull"),
+        ModelPredicateProviderRegistry.register(bow, new Identifier("pull"),
                 (stack, world, entity, seed) -> {
             if (entity == null) {
                 return 0.0f;
@@ -24,7 +23,7 @@ public class ModelsPredicateProviderRegistry {
             return (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / 20.0f;
         });
 
-        ModelPredicateProviderRegistry.register(Items.BOW, new Identifier("pulling"),
+        ModelPredicateProviderRegistry.register(bow, new Identifier("pulling"),
                 (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0f : 0.0f);
     }
 }
