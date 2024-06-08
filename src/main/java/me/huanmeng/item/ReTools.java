fdbe1ea1b12f756be11aa17e0e6be98b48f4f0b0
HuanMeng-official <3099044113@qq.com>
2024-06-08 14:14:27 +0800

    适配极限工具

--- src/main/java/me/huanmeng/item/ReTools.java
+++ src/main/java/me/huanmeng/item/ReTools.java
@@ -18,11 +18,11 @@ public class ReTools {
     public static final Item AMETHYST_SHOVEL;
     public static final Item AMETHYST_HOE;
     public static final Item AMETHYST_SWORD;
-//    public static final Item ULTIMATE_AXE;
-//    public static final Item ULTIMATE_PICKAXE;
-//    public static final Item ULTIMATE_SHOVEL;
-//    public static final Item ULTIMATE_HOE;
-//    public static final Item ULTIMATE_SWORD;
+    public static final Item ULTIMATE_AXE;
+    public static final Item ULTIMATE_PICKAXE;
+    public static final Item ULTIMATE_SHOVEL;
+    public static final Item ULTIMATE_HOE;
+    public static final Item ULTIMATE_SWORD;
 //    public static final Item EMERALD_AXE;
 //    public static final Item EMERALD_PICKAXE;
 //    public static final Item EMERALD_SHOVEL;
@@ -42,12 +42,12 @@ public class ReTools {
         AMETHYST_HOE = registerTools("amethyst_hoe", new HoeItem(ReItemMaterial.AMETHYST, new Item.Settings().attributeModifiers(HoeItem.createAttributeModifiers(ReItemMaterial.AMETHYST, -2, 0.0F))));
         AMETHYST_SWORD = registerTools("amethyst_sword", new SwordItem(ReItemMaterial.AMETHYST, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ReItemMaterial.AMETHYST, 3, -2.4F))));
 
-//        ULTIMATE_AXE = registerTools("ultimate_axe", new AxeItem(ItemMaterial.TITANIUM, 9, -3.0F, new FabricItemSettings()));
-//        ULTIMATE_PICKAXE = registerTools("ultimate_pickaxe", new PickaxeItem(ItemMaterial.TITANIUM, 4, -2.8F, new FabricItemSettings()));
-//        ULTIMATE_SHOVEL = registerTools("ultimate_shovel", new ShovelItem(ItemMaterial.TITANIUM, 4.5F, -3.0F, new FabricItemSettings()));
-//        ULTIMATE_HOE = registerTools("ultimate_hoe", new HoeItem(ItemMaterial.TITANIUM, -3, 0.0F, new FabricItemSettings()));
-//        ULTIMATE_SWORD = registerTools("ultimate_sword", new SwordItem(ItemMaterial.TITANIUM, 6, -2.4F, new FabricItemSettings()));
-//
+        ULTIMATE_AXE = registerTools("ultimate_axe", new AxeItem(ReItemMaterial.TITANIUM, new Item.Settings().attributeModifiers(AxeItem.createAttributeModifiers(ReItemMaterial.TITANIUM, 9, -3.0F))));
+        ULTIMATE_PICKAXE = registerTools("ultimate_pickaxe", new PickaxeItem(ReItemMaterial.TITANIUM, new Item.Settings().attributeModifiers(PickaxeItem.createAttributeModifiers(ReItemMaterial.TITANIUM, 4, -2.8F))));
+        ULTIMATE_SHOVEL = registerTools("ultimate_shovel", new ShovelItem(ReItemMaterial.TITANIUM, new Item.Settings().attributeModifiers(ShovelItem.createAttributeModifiers(ReItemMaterial.TITANIUM, 4.5F, -3.0F))));
+        ULTIMATE_HOE = registerTools("ultimate_hoe", new HoeItem(ReItemMaterial.TITANIUM, new Item.Settings().attributeModifiers(HoeItem.createAttributeModifiers(ReItemMaterial.TITANIUM, -3, 0.0F))));
+        ULTIMATE_SWORD = registerTools("ultimate_sword", new SwordItem(ReItemMaterial.TITANIUM, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ReItemMaterial.TITANIUM, 6, -2.4F))));
+
 //        EMERALD_AXE = registerTools("emerald_axe", new AxeItem(ItemMaterial.EMERALD, 8, -3.0F, new FabricItemSettings()));
 //        EMERALD_PICKAXE = registerTools("emerald_pickaxe", new PickaxeItem(ItemMaterial.EMERALD, 3, -2.8F, new FabricItemSettings()));
 //        EMERALD_SHOVEL = registerTools("emerald_shovel", new ShovelItem(ItemMaterial.EMERALD, 3.5F, -3.0F, new FabricItemSettings()));
