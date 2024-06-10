de4d439eaf831d2abb1ff5f148586a5167f3059d
HuanMeng-official <3099044113@qq.com>
2024-06-10 15:02:36 +0800

    添加RespawnEye的大部分物品

--- src/main/java/me/huanmeng/respawneye/group/ItemsGroup.java
+++ src/main/java/me/huanmeng/respawneye/group/ItemsGroup.java
@@ -20,6 +20,36 @@ public class ItemsGroup {
                     .displayItems(((pParameters, pOutput) -> {
                         pOutput.accept(ReItems.RUBY.get());
                         pOutput.accept(ReItems.SALT.get());
+                        pOutput.accept(ReItems.BASILISK_SCALE.get());
+                        pOutput.accept(ReItems.CATERKILLER_JAWS.get());
+                        pOutput.accept(ReItems.CRYSTAL_SHARD.get());
+                        pOutput.accept(ReItems.DEAD_STINK_BUG.get());
+                        pOutput.accept(ReItems.EMPEROR_SCORPION_SCALE.get());
+                        pOutput.accept(ReItems.GREEN_GOO.get());
+                        pOutput.accept(ReItems.JUMPY_BUG_SCALE.get());
+                        pOutput.accept(ReItems.KRAKEN_TOOTH.get());
+                        pOutput.accept(ReItems.MANTIS_CLAW.get());
+                        pOutput.accept(ReItems.MOBZILLA_SCALE.get());
+                        pOutput.accept(ReItems.MOLENOID_NOSE.get());
+                        pOutput.accept(ReItems.MOTH_SCALE.get());
+                        pOutput.accept(ReItems.NIGHTMARE_SCALE.get());
+                        pOutput.accept(ReItems.PEACOCK_FEATHER.get());
+                        pOutput.accept(ReItems.PINK_TOURMALINE_INGOT.get());
+                        pOutput.accept(ReItems.QUEEN_SCALE.get());
+                        pOutput.accept(ReItems.SEA_MONSTER_SCALE.get());
+                        pOutput.accept(ReItems.SEA_VIPER_TONGUE.get());
+                        pOutput.accept(ReItems.TIGERS_EYE_INGOT.get());
+                        pOutput.accept(ReItems.RAW_TIGERS_EYE.get());
+                        pOutput.accept(ReItems.RAW_TITANIUM.get());
+                        pOutput.accept(ReItems.TITANIUM_INGOT.get());
+                        pOutput.accept(ReItems.TITANIUM_NUGGET.get());
+                        pOutput.accept(ReItems.TREX_TOOTH.get());
+                        pOutput.accept(ReItems.RAW_URANIUM.get());
+                        pOutput.accept(ReItems.URANIUM_INGOT.get());
+                        pOutput.accept(ReItems.URANIUM_NUGGET.get());
+                        pOutput.accept(ReItems.WORM_TOOTH.get());
+                        pOutput.accept(ReItems.VORTEX_EYE.get());
+                        pOutput.accept(ReItems.WATER_DRAGON_SCALE.get());
                     }))
                     .build());
 
--- src/main/java/me/huanmeng/respawneye/item/ReItems.java
+++ src/main/java/me/huanmeng/respawneye/item/ReItems.java
@@ -11,9 +11,72 @@ public class ReItems {
 
     public static final DeferredItem<Item> RUBY;
     public static final DeferredItem<Item> SALT;
+    public static final DeferredItem<Item> BASILISK_SCALE;
+    public static final DeferredItem<Item> CATERKILLER_JAWS;
+    public static final DeferredItem<Item> CRYSTAL_SHARD;
+    public static final DeferredItem<Item> DEAD_STINK_BUG;
+    public static final DeferredItem<Item> EMPEROR_SCORPION_SCALE;
+    public static final DeferredItem<Item> GREEN_GOO;
+    public static final DeferredItem<Item> JUMPY_BUG_SCALE;
+    public static final DeferredItem<Item> KRAKEN_TOOTH;
+    public static final DeferredItem<Item> MANTIS_CLAW;
+    public static final DeferredItem<Item> MOBZILLA_SCALE;
+    public static final DeferredItem<Item> MOLENOID_NOSE;
+    public static final DeferredItem<Item> MOTH_SCALE;
+    public static final DeferredItem<Item> NIGHTMARE_SCALE;
+    public static final DeferredItem<Item> PEACOCK_FEATHER;
+    public static final DeferredItem<Item> PINK_TOURMALINE_INGOT;
+    public static final DeferredItem<Item> QUEEN_SCALE;
+    public static final DeferredItem<Item> SEA_MONSTER_SCALE;
+    public static final DeferredItem<Item> SEA_VIPER_TONGUE;
+    public static final DeferredItem<Item> TIGERS_EYE_INGOT;
+    public static final DeferredItem<Item> RAW_TIGERS_EYE;
+    public static final DeferredItem<Item> RAW_TITANIUM;
+    public static final DeferredItem<Item> TITANIUM_INGOT;
+    public static final DeferredItem<Item> TITANIUM_NUGGET;
+    public static final DeferredItem<Item> TREX_TOOTH;
+    public static final DeferredItem<Item> RAW_URANIUM;
+    public static final DeferredItem<Item> URANIUM_INGOT;
+    public static final DeferredItem<Item> URANIUM_NUGGET;
+    public static final DeferredItem<Item> VORTEX_EYE;
+    public static final DeferredItem<Item> WATER_DRAGON_SCALE;
+    public static final DeferredItem<Item> WORM_TOOTH;
+    public static final DeferredItem<Item> SPARK_FISH;
+
     static {
         RUBY = ITEMS.registerSimpleItem("ruby", new Item.Properties());
         SALT = ITEMS.registerSimpleItem("salt", new Item.Properties());
+        BASILISK_SCALE = ITEMS.registerSimpleItem("basilisk_scale", new Item.Properties());
+        CATERKILLER_JAWS = ITEMS.registerSimpleItem("caterkiller_jaws", new Item.Properties());
+        CRYSTAL_SHARD = ITEMS.registerSimpleItem("crystal_shard", new Item.Properties());
+        DEAD_STINK_BUG = ITEMS.registerSimpleItem("dead_stink_bug", new Item.Properties());
+        EMPEROR_SCORPION_SCALE = ITEMS.registerSimpleItem("emperor_scorpion_scale", new Item.Properties());
+        GREEN_GOO = ITEMS.registerSimpleItem("green_goo", new Item.Properties());
+        JUMPY_BUG_SCALE  = ITEMS.registerSimpleItem("jumpy_bug_scale", new Item.Properties());
+        KRAKEN_TOOTH = ITEMS.registerSimpleItem("kraken_tooth", new Item.Properties());
+        MANTIS_CLAW = ITEMS.registerSimpleItem("mantis_claw", new Item.Properties());
+        MOBZILLA_SCALE  = ITEMS.registerSimpleItem("mobzilla_scale", new Item.Properties());
+        MOLENOID_NOSE = ITEMS.registerSimpleItem("molenoid_nose", new Item.Properties());
+        MOTH_SCALE = ITEMS.registerSimpleItem("moth_scale", new Item.Properties());
+        NIGHTMARE_SCALE  = ITEMS.registerSimpleItem("nightmare_scale", new Item.Properties());
+        PEACOCK_FEATHER = ITEMS.registerSimpleItem("peacock_feather", new Item.Properties());
+        PINK_TOURMALINE_INGOT = ITEMS.registerSimpleItem("pink_tourmaline_ingot", new Item.Properties());
+        QUEEN_SCALE = ITEMS.registerSimpleItem("queen_scale", new Item.Properties());
+        SEA_MONSTER_SCALE  = ITEMS.registerSimpleItem("sea_monster_scale", new Item.Properties());
+        SEA_VIPER_TONGUE = ITEMS.registerSimpleItem("sea_viper_tongue", new Item.Properties());
+        TIGERS_EYE_INGOT = ITEMS.registerSimpleItem("tigers_eye_ingot", new Item.Properties());
+        RAW_TIGERS_EYE  = ITEMS.registerSimpleItem("raw_tigers_eye", new Item.Properties());
+        RAW_TITANIUM = ITEMS.registerSimpleItem("raw_titanium", new Item.Properties());
+        TITANIUM_INGOT  = ITEMS.registerSimpleItem("titanium_ingot", new Item.Properties());
+        TITANIUM_NUGGET  = ITEMS.registerSimpleItem("titanium_nugget", new Item.Properties());
+        TREX_TOOTH = ITEMS.registerSimpleItem("trex_tooth", new Item.Properties());
+        RAW_URANIUM = ITEMS.registerSimpleItem("raw_uranium", new Item.Properties());
+        URANIUM_INGOT  = ITEMS.registerSimpleItem("uranium_ingot", new Item.Properties());
+        URANIUM_NUGGET  = ITEMS.registerSimpleItem("uranium_nugget", new Item.Properties());
+        VORTEX_EYE  = ITEMS.registerSimpleItem("vortex_eye", new Item.Properties());
+        WATER_DRAGON_SCALE  = ITEMS.registerSimpleItem("water_dragon_scale", new Item.Properties());
+        WORM_TOOTH = ITEMS.registerSimpleItem("worm_tooth", new Item.Properties());
+        SPARK_FISH = ITEMS.registerSimpleItem("spark_fish", new Item.Properties());
     }
 
     public static void registryItems(IEventBus eventBus) {
