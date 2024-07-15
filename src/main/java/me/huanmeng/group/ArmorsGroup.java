e76a1ca7a3b190d14820dddff5eed691fc70d676
HuanMeng-official <3099044113@qq.com>
2024-07-15 16:31:05 +0800

    add: added armors' group

---
+++ src/main/java/me/huanmeng/group/ArmorsGroup.java
@@ -0,0 +1,38 @@
+package me.huanmeng.group;
+
+import me.huanmeng.RespawnEye;
+import me.huanmeng.item.ReArmors;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ArmorsGroup {
+
+    public static final ItemGroup ARMORS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(RespawnEye.MODID, "armors_group"),
+            FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ReArmors.RUBY_CHESTPLATE))
+                    .displayName(Text.translatable("ig.respawneye.armor"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ReArmors.RUBY_HELMET);
+                        entries.add(ReArmors.RUBY_CHESTPLATE);
+                        entries.add(ReArmors.RUBY_LEGGINGS);
+                        entries.add(ReArmors.RUBY_BOOTS);
+                        entries.add(ReArmors.AMETHYST_HELMET);
+                        entries.add(ReArmors.AMETHYST_CHESTPLATE);
+                        entries.add(ReArmors.AMETHYST_LEGGINGS);
+                        entries.add(ReArmors.AMETHYST_BOOTS);
+                        entries.add(ReArmors.EMERALD_HELMET);
+                        entries.add(ReArmors.EMERALD_CHESTPLATE);
+                        entries.add(ReArmors.EMERALD_LEGGINGS);
+                        entries.add(ReArmors.EMERALD_BOOTS);
+                    })
+                    .build());
+
+    public static void registryGroup() {
+    }
+}
