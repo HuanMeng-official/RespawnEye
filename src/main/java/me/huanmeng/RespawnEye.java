5be8a6e447f9d8cee3479d3d99e75afc9754979b
HuanMeng-official <3099044113@qq.com>
2024-08-04 17:25:42 +0800

    modify: change the logger name

--- src/main/java/me/huanmeng/RespawnEye.java
+++ src/main/java/me/huanmeng/RespawnEye.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RespawnEye implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("respawneye");
+    public static final Logger LOGGER = LoggerFactory.getLogger("RespawnEye");
 	public static final String MODID = "respawneye";
 
 	@Override
